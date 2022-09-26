8843452beb14a9c1fb75ba86ade99e339343296a
yuyingzhang <yuyi0011@stud.kea.dk>
2022-09-26 14:47:08 +0200

    Adventure2022

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        UserInteface vi = new UserInterfacce();
+        vi.start();
+    }
+    UserInterface;
+}
