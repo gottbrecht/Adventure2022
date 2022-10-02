f352e20825da3c82c48ed9d18148c9ffdccb9656
gottbrecht <simo79j5@stud.kea.dk>
2022-10-02 07:19:41 -0700

    toString

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -6,17 +6,34 @@ public class Item {
     private String itemList;
     private String itemName;
 
+
+    private String cash;
+    private String goldenWheels;        //Items could be anything. I just chose some.
+    private String carPolish;
+    private String dogBites; //for the wolves
+
     public String getItem() {
         return item;
     }
     public String getItemList() {
         return itemList;
     }
-    private Item (String item, String itemList, String itemName) {
+    private Item (String item, String itemList, String itemName, String cash, String goldenWheels, String carPolish) {
         this.item = item;
         this.itemList = itemList;
         this.itemName = itemName;
+        this.cash = cash;
+        this.goldenWheels = goldenWheels;
+        this.carPolish = carPolish;
+
     }
 
-    //private final ArrayList<Item> itemList = new ArrayList<>();
+    private final ArrayList<Item> Item = new ArrayList<>();
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "item='" + item + "itemList=" + itemList + "itemName='" + itemName;
+
+    }
 }
